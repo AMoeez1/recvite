ad3be451fe05d70b21a5e2c5bed0ca79620df0bc
AMoeez1 <mujjeequraishi@gmail.com>
2025-06-14 21:38:36 +0500

    services image banner added

--- src/components/Blog/Title/Title.jsx
+++ src/components/Blog/Title/Title.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './title.css'
 const PageTitle = ({ text, title, desc, detail }) => {
   return (
     // Page Title
     <section
       className="page-title centred"
       style={{
-        backgroundImage: "url(assets/images/background/page-title.jpg)",
+        backgroundImage: 'url("/assets/images/web images/services/breadservice.jpg")',
+        minHeight: '100vh',
       }}
     >
       <div className="auto-container">
         <div className="content-box">
           <div className="title">
-            <h1>{title}</h1>
+            <h1 className="title" >{title}</h1>
           </div>
           {text && (
             <ul className="bread-crumb clearfix">
-              <li>
+              {/* <li>
                 <Link to="/">Home</Link>
-              </li>
+              </li> */}
               <li>{text}</li>
             </ul>
           )}
--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -9,23 +9,27 @@ import PageTitle from "../../components/Blog/Title/Title";
 import Footer from "../../components/Footer/Footer";
 import ServicePageSection from "../../components/Service/Service-section/ServiceSection";
 import SectionThree from "../../components/Home/section3/SectionThree";
-import ServiceSectionOne from "../../components/Service/section1/ServiceSectionOne"
+import ServiceSectionOne from "../../components/Service/section1/ServiceSectionOne";
 const Service = () => {
   return (
     <div className="page-wrapper">
       {/* <div class="preloader"></div> */}
       <Header />
       <MobileMenu />
-      <PageTitle title={'Our Solutions'} text={'Our Solutions'} />
+      <PageTitle
+        title={"Recruitment & Development"}
+        text={
+          "Since the exceptionally starting of our commerce, we’ve been interfacing our clients with difficult discoverability within the Data Innovation space. Our recruitment consulting firm ability ranges over a wide cluster of subdomains counting Recruitment & Development"
+        }
+      />
 
-      <SectionThree serv/>
-      <ServiceSectionOne/>
-      <SectionThree serv/>
-      <ServiceSectionOne/>
-      <SectionThree serv/>
+      <SectionThree serv />
+      <ServiceSectionOne />
+      <SectionThree serv />
+      <ServiceSectionOne />
+      <SectionThree serv />
 
-    {/* <ServicePageSection /> */}
-      
+      {/* <ServicePageSection /> */}
 
       <Footer />
 
